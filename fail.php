27fddc56a6c5ed56522b1f7946f5440e82ca28a5
sagarsavla@gmail.com <sagarsavla@gmail.com@108b678c-7939-f90a-a620-80a12f88a414>
2011-12-10 13:53:34 +0000

    s

---
+++ fail.php
@@ -0,0 +1,35 @@
+<?php
+/*
+
+<!--
+This piece of awesomeness... 
+	...dedicated to
+					V.K.S.
+	...wrought by 
+					Sagar Savla 
+						http://sagz.in
+-->
+
+*/
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>
+      SQL Injection Demo
+    </title>
+  </head>
+ <!--
+
+This piece of awesomeness wrought by:
+
+Sagar Savla - http://sagz.in (dedicated to V.K.S.)
+-->
+  <body>
+    <h1 align="center">
+      Failure!
+    </h1>
+  </body>
+</html>
