7e2500db661a513ede3918e96e18344d030844ed
sagarsavla <sagarsavla@gmail.com>
2011-12-10 13:54:00 +0000

    s

---
+++ normprocess.php
@@ -0,0 +1,40 @@
+<?php
+        //Start session
+        //Include database connection details
+            require 'dbase.php';
+        
+        $userfound=false;
+        
+        //Connect to mysql server
+        
+        //Function to sanitize values received from the form. Prevents SQL injection
+                function clean($str) {
+                $str = @trim($str);
+                if(get_magic_quotes_gpc()) {
+                        $str = stripslashes($str);
+                }
+                return mysql_real_escape_string($str);
+        }       
+		
+		//Sanitize the POST values
+        $userid = $_POST['user'];
+        $pass = $_POST['pass'];
+        
+		
+		//Check for duplicate 
+                $qry2 = "SELECT * FROM usert WHERE id='sakec' and pass='$pass'";
+                $result2 = mysql_query($qry2);
+                if($result2) 
+                {
+                        if(mysql_num_rows($result2) > 0) {
+                                $uid=$result2['uid'];
+                                $userfound=true;
+                        }
+                }
+
+		if($userfound)
+			die("Sucess");
+		else
+			die("Fail");
+        
+?>
