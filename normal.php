a4fbbd605cc3463fb7bfc8513b60d20efdeb7170
sagarsavla@gmail.com <sagarsavla@gmail.com@108b678c-7939-f90a-a620-80a12f88a414>
2011-12-10 13:53:50 +0000

    s

---
+++ normal.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>
+      SQL Injection Demo
+    </title>
+  </head>
+ <!--
+
+This piece of awesomeness wrought by:
+
+Sagar Savla - http://sagz.in (dedicated to V.K.S.)
+-->
+  <body>
+    <h1 align="center">
+      Normal vulnerable form
+    </h1><br>
+    <form name="cont" method="post" action="normprocess.php" id="cont">
+      <center>
+        <table>
+          <tr>
+            <td align="right">
+               <input type="reset" value="Clear"> Username:
+            </td>
+            <td>
+              <input type="text" required name="user" placeholder="User id" maxlength="10" size = "15" >
+            </td>
+          </tr>
+          <tr>
+            <td align="right">
+              Password:
+			  <br/>
+			  <br/>
+            </td>
+            <td>
+              <input id="pass" type="text" required placeholder="Password" name="pass" maxlength="20" size " 15 "><br/><br/>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td><br/>
+              <input type="submit" value="Submit Information">
+            </td>
+          </tr>
+        </table>
+      </center>
+    </form>
+  </body>
+</html>
